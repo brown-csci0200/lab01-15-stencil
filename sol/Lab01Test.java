565140a00d302c3a91b4e022ee62ff158ac946bc
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-01-29 22:13:47 -0500

    Initial commit.

---
+++ sol/Lab01Test.java
@@ -0,0 +1,23 @@
+package sol;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import src.FuncList;
+
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lab01Test {
+
+    private FuncList toFuncList(Object... args) {
+        FuncList FuncListBuilder = new FuncList();
+        for (int i = args.length - 1; i >= 0; i--) {
+            FuncListBuilder = FuncListBuilder.link(args[i]);
+        }
+        return FuncListBuilder;
+    }
+
+}
